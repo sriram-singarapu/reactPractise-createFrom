6bda40ca082616f80750a4ac4396ed84479b72dc
sriram057 <sriramsingarapu2@gmail.com>
2024-04-05 19:31:02 +0530

    lists are rendered

--- my-app/src/components/Users/UserList/UserList.js
+++ my-app/src/components/Users/UserList/UserList.js
@@ -7,7 +7,7 @@ const UserList = (props) => {
     <Card className="users">
       <ul>
         {props.users.map((user) => (
-          <li>
+          <li key={user.id}>
             {user.name} ({user.age}years old)
           </li>
         ))}
